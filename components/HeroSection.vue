38acce008d24a06d8625230fdfa4afef9ee9c436
vvsubash <subash.venkat007@gmail.com>
2020-03-23 06:34:06 +0530

    feat: added modal to confirm.
    need to think of ui &ux.

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -6,7 +6,22 @@
       </h2>
     </div>
     <div class="sm-12 md-4 lg-2 col">
-      <button>Click to Subscribe</button>
+      <div class="row flex-spaces child-borders">
+        <label class="paper-btn margin" for="modal-1">Click it now</label>
+      </div>
+      <input id="modal-1" class="modal-state" type="checkbox" />
+      <div class="modal">
+        <label class="modal-bg" for="modal-1"></label>
+        <div class="modal-body">
+          <label class="btn-close" for="modal-1">X</label>
+          <h4 class="modal-title">Modal Title</h4>
+          <h5 class="modal-subtitle">Modal Subtitle</h5>
+          <p class="modal-text">
+            This is an example of modal which is implemented with pure CSS! :D
+          </p>
+          <label for="modal-1">Nice!</label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
