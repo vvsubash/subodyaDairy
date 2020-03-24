57b0a487e17cdc4eed6f1707d1a3d84c3f9359d6
venkat <subash.venkat007@gmail.com>
2020-03-24 21:33:32 +0530

    feat: adding userstatemodal

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -6,31 +6,17 @@
       </h2>
     </div>
     <div class="sm-12 md-4 lg-2 col">
-      <div class="row flex-spaces">
-        <label class="paper-btn" for="modal-1">Click it now</label>
-      </div>
-      <input id="modal-1" class="modal-state" type="checkbox" />
-      <div class="modal">
-        <label class="modal-bg" for="modal-1"></label>
-        <div class="modal-body">
-          <label class="btn-close" for="modal-1">X</label>
-          <h4 class="modal-title">Sign in</h4>
-          <h5 class="modal-subtitle">
-            Please Signin So we can serve you better
-          </h5>
-          <reactive-nav-button class="paper-btn margin" />
-        </div>
-      </div>
+      <UserStateModal />
     </div>
   </div>
 </template>
 
 <script>
-import ReactiveNavButton from '~/components/ReactiveNavButton.vue'
+import UserStateModal from '~/components/UserStateModal.vue'
 
 export default {
   components: {
-    ReactiveNavButton
+    UserStateModal
   }
 }
 </script>
