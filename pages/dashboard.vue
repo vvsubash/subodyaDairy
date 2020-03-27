45df4aa87cccb0552c00a4bee0dd1cd9d587ece8
venkat <subash.venkat007@gmail.com>
2020-03-27 08:16:16 +0530

    feat: MHB

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,15 +1,56 @@
 <template>
-  <div class="container">
+  <div>
     <p>Hello {{ this.$store.state.user }} continue</p>
     <form class="form-group border col">
-      <label for="paperInputs1">Name</label>
-      <input type="text" placeholder="Nice input" />
+      <div class="form-group">
+        <label for="paperInputs1">Name</label>
+        <input v-model="userName" class="input-block" />
+      </div>
+      <div class="form-group">
+        <label for="paperInputs1">Phone Number</label>
+        <input v-model="phoneNumber" class="input-block" />
+      </div>
+      <div class="form-group">
+        <h3>Address</h3>
+        <label for="paperInputs1">Door Number</label>
+        <input v-model="phoneNumber" class="input-block" />
+        <label for="paperInputs1">Street Name</label>
+        <input v-model="phoneNumber" class="input-block" />
+      </div>
+      <div>
+        <label for="paperInputs1">Pin Code</label>
+        <input v-model="pinCode" class="input-block" />
+      </div>
+      <div v-if="pinCode == 530003">
+        .
+        <fieldset class="form-group">
+          <legend>Token payment</legend>
+          <label for="paperChecks1" class="paper-check">
+            <input
+              id="paperChecks1"
+              type="checkbox"
+              name="paperChecks"
+              value="option 1"
+            />
+            <span>I Agree to make a token payment</span>
+          </label>
+        </fieldset>
+      </div>
+      <input type="submit" value="Submit & Get Subscribed" />
     </form>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      userName: this.$store.state.user,
+      phoneNumber: 9123451234,
+      pinCode: 530003
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
