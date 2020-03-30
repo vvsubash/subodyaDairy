480fbe9f1e9def83d82b521112db889e712b3987
vvsubash <subash.venkat007@gmail.com>
2020-03-30 21:58:01 +0530

    OOOK

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="home">
-    <form>
-      <input v-model="price" type="text" />
-      <button @click="makePayment">GO</button>
-    </form>
+  <div>
+    <input v-model="price" type="text" />
+    <button @click="makePayment">GO</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  components: {},
   data() {
     return {
       price: 0
@@ -19,16 +16,15 @@ export default {
   methods: {
     // Initializing the payment request
     makePayment() {
-      // 1. GENERATE ORDER_ID using razorpay/order api
       axios
         .post('https://us-central1-ssss-ss.cloudfunctions.net/sap/', {
-          amount: this.price
+          amount: this.price * 100
         })
-        .then((res) => {
+        .then((response) => {
           // eslint-disable-next-line no-console
-          console.log(res)
+          console.log(response)
           // eslint-disable-next-line no-undef
-          const rzp1 = new Razorpay({ ...options, order_id: res.data.id })
+          const rzp1 = new Razorpay({ ...options, order_id: response.data.id })
           rzp1.open()
         })
         .catch((err) => {
@@ -41,7 +37,7 @@ export default {
       var options = {
         key: 'rzp_test_vLUpWWam4Z8FgN',
         amount: '59900', /// The amount is shown in currency subunits. Actual amount is ₹599.
-        name: 'Formec Media',
+        name: 'Subodaya Dairy',
         currency: 'INR', // Optional. Same as the Order currency
         description: 'Purchase Description',
         image: 'https://jolly-volhard-bc2f0b.netlify.com/favicon.ico',
@@ -49,8 +45,8 @@ export default {
           this.verifySignature(response)
         },
         prefill: {
-          name: 'Rohan Lamb',
-          email: 'lambrohan@gmail.com'
+          name: 'Venkat',
+          email: 'venkat@suba.sh'
         },
         notes: {
           address: 'Hello World'
@@ -61,37 +57,18 @@ export default {
       }
     },
     verifySignature(response) {
-      // eslint-disable-next-line no-console
-      console.log(response)
-      // axios
-      //   .post(
-      //     'https://us-central1-paytm-payments-f3957.cloudfunctions.net/paymentApi/confirmPayment',
-      //     response
-      //   )
-      //   .then((res) => {
-      //     // eslint-disable-next-line no-console
-      //     console.log('PAYMENT RESPONSE', res)
-      //   })
-      //   // eslint-disable-next-line handle-callback-err
-      //   .catch((err) => {
-      //     // eslint-disable-next-line no-console
-      //     console.log('error')
-      //   })
+      axios
+        .post('https://us-central1-ssss-ss.cloudfunctions.net/csap/', response)
+        .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log('PAYMENT RESPONSE', res.data)
+        })
+        // eslint-disable-next-line handle-callback-err
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.log('error')
+        })
     }
   }
 }
 </script>
-
-<style scoped>
-input {
-  border: 1px solid rgb(0, 217, 255);
-  padding: 8px 40px;
-  text-align: center;
-}
-button {
-  background: rgb(0, 241, 161);
-  border: none;
-  padding: 8px 30px;
-  margin: 16px;
-}
-</style>
