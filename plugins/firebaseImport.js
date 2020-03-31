6fe46bd776e2b3b123363ad973b820a52ac1487c
vvsubash <subash.venkat007@gmail.com>
2020-03-31 11:06:23 +0530

    made it modern and changed default import of firebase

--- plugins/firebaseImport.js
+++ plugins/firebaseImport.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app'
+import * as firebase from 'firebase/app'
+
 import 'firebase/firestore'
 
 const firebaseApp = firebase.initializeApp({
--- store/index.js
+++ store/index.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
+
+import 'firebase/auth'
+
 // import { vuexfireMutations, firestoreAction } from 'vuexfire'
 // import db from '~/plugins/firebaseImport'
 export const state = () => ({
