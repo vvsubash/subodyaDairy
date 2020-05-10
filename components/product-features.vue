55684acb03f2e827f82c67288d12f427f3304eff
venkat <subash.venkat007@gmail.com>
2020-05-10 09:31:41 +0530

    added  product feature

---
+++ components/product-features.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row flex-middle flex-center">
+    <div class="sm-6 md-5 lg-2 col">
+      <img src="https://picsum.photos/300/2g00" alt="reusableb2" />
+    </div>
+    <div class="sm-12 md-7 col">
+      <h2>
+        Can I get skimmed or pasturised milk
+      </h2>
+      <h4>
+        No, We wouldn't adultrate milk so we can't supply them. We are not
+        equiped for that and we also feel that milk is to be consumed as nature
+        intended to. So Fat, S.N.F, texture of milk is affected by many factors
+        beyond our control. various measuremnts regarding quality milk will be
+        reflected in your statement
+      </h4>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped></style>
